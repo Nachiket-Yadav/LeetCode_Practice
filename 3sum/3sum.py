20b0695a4a619a4684c4d7caabe78f1ae353fced
Nachiket-Yadav <nachiyadav@utexas.edu>
2026-07-10 17:55:00 -0500

    optimized 3sum.py, same optimization not yet applied to 3sum.cpp

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -4,6 +4,9 @@ class Solution:
         nums.sort()
 
         for i in range(len(nums)):
+            if nums[i] > 0:
+                break  # if the smallest remaining number is non-zero then nothing will add up to zero
+
             if i  > 0 and nums[i] == nums[i-1]:
                 continue # If the next number is the same as the one we just went over we'll just find the same solution
             
@@ -15,9 +18,12 @@ class Solution:
                 if current_sum == 0:
                     sols.append([nums[i] , nums[j] , nums[k]])
                     j += 1
+                    k -= 1
 
-                    while nums[j] == nums[j-1] and j < k:
+                    while j < k and nums[j] == nums[j-1]: 
                         j += 1
+                    while j < k and nums[k] == nums[k+1]: 
+                        k -= 1
                 else:
                     if current_sum < 0: 
                         j += 1 # since nums is sorted moving the left pointer to a bigger index so that the sum becomes larger
